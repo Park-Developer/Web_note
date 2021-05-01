2fafe38f968a988cd0db780f578a0516faeecae0
Park-Developer <parkwonho94@gmail.com>
2021-05-01 23:05:17 +0900

    Personal Project Start!

---
+++ Javascript/clock.js
@@ -0,0 +1,29 @@
+const clockContainer=document.querySelector(".status-bar__Clock"),
+clockDate =clockContainer.querySelector(".status-bar__Clock_Date"),
+clockTime =clockContainer.querySelector(".status-bar__Clock_Time");
+
+
+
+
+
+
+function getTime(){
+    const date = new Date();
+    const minutes = date.getMinutes();
+    const hours = date.getHours();
+    const seconds= date.getSeconds()
+    
+    const month = date.getMonth()
+    const day = date.getDay();
+    const year = date.getUTCFullYear();
+    clockDate.innerText=`${year}-${month}-${day}`;
+    clockTime.innerText=`${hours}:${minutes}:${ seconds<10 ? `0${seconds}`: seconds}`;
+  }  
+  
+  function init(){
+  getTime();
+  setInterval(getTime,1000);
+  }
+  
+  init();
+  
