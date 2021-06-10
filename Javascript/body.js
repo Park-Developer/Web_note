fb922dd67fef668e878e4e8b5f911080b28b7e88
Park-Developer <parkwonho94@gmail.com>
2021-06-10 15:58:22 +0900

    잔버그 수정

--- Javascript/body.js
+++ Javascript/body.js
@@ -1,7 +1,3 @@
-let body_click_Non_application_range=["js-Memo_MD_Display","js-Memo_contents",
-"js-Memo__Button_1","js-Memo__Button_2","js-Memo__Button_3"];
-
-
 function body_click_event(){
     console.log("body_click_event",event.target);
     
@@ -11,13 +7,11 @@ function body_click_event(){
     } else{
     
     }
-    
 }
 
 // Event Setting
 function init(){
     document.body.addEventListener('click', body_click_event);  
 }
-
 
 init();
